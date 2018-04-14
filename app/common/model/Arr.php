8f24b6cf1ab414c64a89dfc970e1a5281770b1dc
潇欢 <243484014@qq.com>
2018-04-14 12:57:55 +0800

    更新 Arr.php

--- app/common/model/Arr.php
+++ app/common/model/Arr.php
@@ -1,12 +1,5 @@
 <?php
-/** .-------------------------------------------------------------------
- * |  Software: [HDCMS framework]
- * |      Site: www.hdcms.com
- * |-------------------------------------------------------------------
- * |    Author: 向军 <2300071698@qq.com>
- * |    WeChat: aihoudun
- * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
- * '-------------------------------------------------------------------*/
+
 namespace app\common\model;
 /**
  * 数组管理
